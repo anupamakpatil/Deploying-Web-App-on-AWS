a9f62b5934a39b5abbe790faf4d6c90f59add4c0
Anupama K Patil <79805696+anupamakpatil@users.noreply.github.com>
2023-04-24 22:29:25 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, url_for,request, redirect, flash
+from flask_mysqldb import MySQL
+
+
+app= Flask(__name__)
+app.secret_key="cc_project"
+
+
+app.config['MYSQL_HOST'] = 'database-1.ckuhiu36dj7n.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'root123'
+app.config['MYSQL_DB'] = 'todo'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM tasks ORDER BY id DESC')
+    all_data = cur.fetchall()
+    mysql.connection.commit()
+    return render_template('index.html', all_data=all_data)
+
+@app.route('/submit_form', methods=['POST'])
+def submitted_form():
+    if request.method=='POST':
+        task = request.form['task']
+        cur = mysql.connection.cursor()
+        cur.execute('USE todo;')
+        cur.execute('INSERT INTO tasks (task) VALUES (%s)',[task])
+        mysql.connection.commit()
+        flash("Your task Successfully Saved.")
+        return redirect('/')
+    else:
+        return 'Data is not Submitted.'
+
+@app.route('/delete/<string:task_id>')
+def delete(task_id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM tasks WHERE id = %s',[task_id])
+    mysql.connection.commit()
+    flash('Task Deleted Successfully.')
+    return redirect('/')
+
+
+@app.route('/edit/<string:task_id>')
+def edit_view(task_id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM tasks WHERE id=%s',[task_id])
+    data = cur.fetchone()
+    mysql.connection.commit()
+    return render_template('edit.html',data=data)
+
+@app.route('/update_task', methods=['POST'])
+def update():
+    task_id = request.form['task_id']
+    task_title = request.form['task_name']
+
+    cur = mysql.connection.cursor()
+    cur.execute('UPDATE tasks SET task=%s,created_date=NOW() WHERE id=%s',[task_title,task_id])
+    mysql.connection.commit()
+    flash('Task Updated Successfully.')
+    return redirect('/')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
